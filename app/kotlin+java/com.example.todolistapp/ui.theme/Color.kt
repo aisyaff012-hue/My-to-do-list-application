fd39c006ca172a4c1008ee3616aef79cda9e7e45
aisyaff012-hue <aisyaff012@gmail.com>
2026-09-01 21:00:21 +0800

    Create Color.kt

---
+++ app/kotlin+java/com.example.todolistapp/ui.theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.todolistapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// utama app
+val PrimaryBlue = Color(0xFF2196F3)
+val PrimaryBlueDark = Color(0xFF1976D2)
+
+// untuk task yang dah siap
+val TaskDoneGreen = Color(0xFF4CAF50)
+
+// untuk delete
+val DeleteRed = Color(0xFFEF5350)
+
+// background card
+val CardBackground = Color(0xFFF5F5F5)
+val CardDoneBackground = Color(0xFFE8F5E9)
+
+// text
+val TextGray = Color(0xFF9E9E9E)
